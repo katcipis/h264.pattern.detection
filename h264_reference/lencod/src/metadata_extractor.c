67d161da3233fd2e5e7e180b678bbffbcbe7e112
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-03-22 12:22:33 -0300

    finally no more memory errors

--- h264_reference/lencod/src/metadata_extractor.c
+++ h264_reference/lencod/src/metadata_extractor.c
@@ -80,7 +80,7 @@ ExtractedMetadata ** metadata_extractor_extract_from_yuv(unsigned char ** y, int
   IplImage * frame                   = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 3);
   IplImage * gray                    = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 1);
   CvSeq* results                     = NULL;
-  //ExtractedMetadata ** metadata_objs = NULL;
+  ExtractedMetadata ** metadata_objs = NULL;
 
   int frame_i = 0;
   int row     = 0;
@@ -125,7 +125,8 @@ ExtractedMetadata ** metadata_extractor_extract_from_yuv(unsigned char ** y, int
 
   printf("Found [%d] metadata objects !!!\n", results->total);
 
-  //metadata_objs = malloc(sizeof(ExtractedMetadata *) * (results->total + 1));
+  metadata_objs = malloc(sizeof(ExtractedMetadata *) * (results->total + 1));
+  printf("Allocated [%d]bytes\n", sizeof(ExtractedMetadata *) * (results->total + 1));
 
   /* On this case we will sent the grayscale found object as metadata (uncompressed) */
 
@@ -133,28 +134,29 @@ ExtractedMetadata ** metadata_extractor_extract_from_yuv(unsigned char ** y, int
   
     CvRect* res                  = (CvRect*)cvGetSeqElem( results, i);
     ExtractedMetadata * metadata = metadata_extractor_new_metadata(res->width, res->height);
-    
+    int metadata_row = 0;
+   
     printf("generating metadata[%d]\n", i); 
 
     /* Copy the object from the original frame */
     for (row = res->y; row < (res->height + res->y); row++) {
 
-      int j          = 0;
+      int metadata_col = 0;
 
       for (col = res->x; col < (res->width + res->x); col ++) {
-          metadata->y[i][j] = y[row][col];
-          j++;
+          metadata->y[metadata_row][metadata_col] = y[row][col];
+          metadata_col++;
       }
-
-      i++;
+      metadata_row++;
     }
 
     printf("done generating metadata\n");
-    //*(metadata_objs + i) = NULL;
+    printf("saving metadata [%d]\n", i);
+    metadata_objs[i] = metadata;
   }
  
   /* NULL termination */
-  //metadata_objs[results->total] = NULL;
+  metadata_objs[results->total] = NULL;
 
   /* Freeing images */
   cvReleaseImage(&frame);
