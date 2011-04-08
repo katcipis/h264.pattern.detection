db2d74772d7457bad20c7394f05fb1e014a5a28a
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-04-08 15:08:34 -0300

    adding a new test video

---
+++ h264_reference/bin/tests/coast_guard_ten_seconds/cleanup.sh
@@ -0,0 +1,1 @@
+rm -f coast_guard_qcif.h264 coast_guard_stats.dat rec_coast_guard_qcif.yuv coast_guard_qcif_dec.yuv
---
+++ h264_reference/bin/tests/coast_guard_ten_seconds/play_encoded_video.sh
@@ -0,0 +1,1 @@
+gst-launch-0.10 -v filesrc location=coast_guard_qcif.h264 ! decodebin ! autovideosink
