e0443e75ba9972b51053fdf533d2c8852b687be6
Tiago Katcipis <tiagokatcipis@gmail.com>
2011-04-24 16:41:23 -0300

    changing configuration to encode faster, but quality is crappy

--- h264_reference/bin/tests/akiyo_ten_seconds/play_encoded_video.sh
+++ h264_reference/bin/tests/akiyo_ten_seconds/play_encoded_video.sh
@@ -1,1 +1,1 @@
-gst-launch-0.10 -v filesrc location=akiyo_qcif.h264 ! decodebin ! ffmpegcolorspace ! videoparse format=1 width=176 height=144 framerate=30/1 ! autovideosink
+gst-launch-0.10 -v filesrc location=akiyo_qcif.h264 ! decodebin ! ffmpegcolorspace ! videoparse format=1 width=176 height=144 framerate=30/1 ! ffmpegcolorspace ! videorate ! autovideosink
