ecf3ecc72ea3f0bf7c8f840852f4e3f6585351fc
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-06-12 13:46:59 -0300

    added foreman, need to do some profilling later

--- h264_reference/bin/tests/foreman/play_encoded_video.sh
+++ h264_reference/bin/tests/foreman/play_encoded_video.sh
@@ -1,1 +1,1 @@
-gst-launch-0.10 -v filesrc location=foreman.h264 ! decodebin ! ffmpegcolorspace ! videoparse format=1 width=385 height=288 framerate=30/1 ! ffmpegcolorspace ! videorate ! autovideosink
+gst-launch-0.10 -v filesrc location=foreman.h264 ! decodebin2 ! ffmpegcolorspace ! videorate ! autovideosink
