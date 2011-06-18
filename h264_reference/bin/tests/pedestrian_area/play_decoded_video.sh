03091375affc689eb68ff29fb72c69771d03bf4a
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-06-18 17:55:09 -0300

    fixing pedestrian test

--- h264_reference/bin/tests/pedestrian_area/play_decoded_video.sh
+++ h264_reference/bin/tests/pedestrian_area/play_decoded_video.sh
@@ -1,1 +1,1 @@
-gst-launch-0.10 filesrc location=./foreman_dec.yuv ! videoparse format=1 width=352 height=288 framerate=30/1 ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 filesrc location=./pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! autovideosink
--- h264_reference/bin/tests/pedestrian_area/play_original_video.sh
+++ h264_reference/bin/tests/pedestrian_area/play_original_video.sh
@@ -1,1 +1,1 @@
-gst-launch-0.10 filesrc location=./foreman.yuv ! videoparse format=1 width=352 height=288 framerate=30/1 ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 filesrc location=./pedestrian_area.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! autovideosink
