58f9a0975fbaee89a3e65e6a5caf99b9199e44e0
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-06-19 12:25:18 -0300

    testing with pedestrian

--- h264_reference/bin/tests/pedestrian_area/play_encoded_video.sh
+++ h264_reference/bin/tests/pedestrian_area/play_encoded_video.sh
@@ -1,1 +1,1 @@
-gst-launch-0.10 -v filesrc location=foreman.h264 ! decodebin2 ! ffmpegcolorspace ! videorate ! autovideosink
+gst-launch-0.10 -v filesrc location=pedestrian_area.h264 ! decodebin2 ! ffmpegcolorspace ! videorate ! autovideosink
