226682a40e942f265e0c80924036e17088971e11
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-06-26 14:17:47 -0300

    yes yes

--- h264_reference/bin/tests/speed_bag/get_original_video.sh
+++ h264_reference/bin/tests/speed_bag/get_original_video.sh
@@ -6,4 +6,4 @@ if test ! $? -eq 0; then
   wget http://media.xiph.org/video/derf/y4m/speed_bag_1080p.y4m
 fi
 
-gst-launch -v filesrc location=speed_bag_1080p.y4m ! decodebin2 ! ffmpegcolorspace ! video/x-raw-yuv,format=\(fourcc\)I420 ! filesink location=speed_bag_1080p.yuv
+gst-launch -v filesrc location=speed_bag_1080p.y4m ! decodebin2 ! ffmpegcolorspace ! video/x-raw-yuv,format=\(fourcc\)I420 ! filesink location=speed_bag.yuv
--- h264_reference/bin/tests/speed_bag/play_original_video.sh
+++ h264_reference/bin/tests/speed_bag/play_original_video.sh
@@ -1,1 +1,1 @@
-gst-launch-0.10 filesrc location=./pedestrian_area.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 filesrc location=./speed_bag.yuv ! videoparse format=1 width=1920 height=1080 framerate=30/1 ! ffmpegcolorspace ! autovideosink
