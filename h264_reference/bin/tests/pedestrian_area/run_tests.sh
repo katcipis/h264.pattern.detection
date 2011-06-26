be2a0406623bd59766e9a1e5f5c00ead22ff6bee
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-06-26 14:37:47 -0300

    changing the way things are done :-)

---
+++ h264_reference/bin/tests/pedestrian_area/run_tests.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+file pedestrian_area_1080p25.y4m
+
+if test ! $? -eq 0; then
+    wget http://media.xiph.org/video/derf/y4m/pedestrian_area_1080p25.y4m
+fi
+
+
+echo "=== Generating YUV file ==="
+
+gst-launch -v filesrc location=pedestrian_area_1080p25.y4m ! decodebin2 ! filesink location=pedestrian_area.yuv
+rm pedestrian_area_1080p25.y4m
+
+
+echo "=== Creating directory to put results ==="
+mkdir results
+
+
+echo "=== Starting original test ==="
+
+cd ../../
+nice -n -20 ./lencod.exe -f ./tests/pedestrian_area/encoder.cfg &> ./tests/pedestrian_area/results/encoder_original.res
+
+
+echo "=== Starting 1/1 test ==="
+
+nice -n -20 ./lencod.exe -f ./tests/pedestrian_area/encoder_1_1.cfg &> ./tests/pedestrian_area/results/encoder_1_1.res
+nice -n -20 ./ldecod.exe -f ./tests/pedestrian_area/decoder.cfg
+nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! xvidenc ! avimux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_1_1.avi
+
+
+echo "=== Starting 5/10 test ==="
+
+nice -n -20 ./lencod.exe -f ./tests/pedestrian_area/encoder_5_10.cfg &> ./tests/pedestrian_area/results/encoder_5_10.res
+nice -n -20 ./ldecod.exe -f ./tests/pedestrian_area/decoder.cfg 
+nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! xvidenc ! avimux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_5_10.avi
+
+
+echo "=== Starting 10/30 test ==="
+
+nice -n -20 ./lencod.exe -f ./tests/pedestrian_area/encoder_10_30.cfg &> ./tests/pedestrian_area/results/encoder_10_30.res
+nice -n -20 ./ldecod.exe -f ./tests/pedestrian_area/decoder.cfg 
+nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! xvidenc ! avimux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_10_30.avi
+
+
+echo "=== Starting 10/60 test ==="
+
+nice -n -20 ./lencod.exe -f ./tests/pedestrian_area/encoder_10_60.cfg &> ./tests/pedestrian_area/results/encoder_10_60.res
+nice -n -20 ./ldecod.exe -f ./tests/pedestrian_area/decoder.cfg 
+nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! xvidenc ! avimux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_10_60.avi
+
+echo "=== We are done ==="
