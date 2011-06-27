af8bfb193d942b06ef09cd55b23452cc6a2a86fe
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-06-27 20:50:30 -0300

    still some work to do on the script

--- haar_test/run_tests.sh
+++ haar_test/run_tests.sh
@@ -24,24 +24,38 @@ if test ! $? -eq 0; then
   wget http://media.xiph.org/video/derf/y4m/speed_bag_1080p.y4m
 fi
 
+file blue_sky_1080p25.y4m
+
+if test ! $? -eq 0; then
+  echo "Downloading video Blue Sky 1080"
+  http://media.xiph.org/video/derf/y4m/blue_sky_1080p25.y4m
+fi
+
+mkdir -p results
+
 #Scaling tests
 
 echo ""
+echo "Starting scaling tests"
 echo "Generating 1080p YUV videos"
 echo ""
 
 gst-launch -v filesrc location=crowd_run_1080p50.y4m ! decodebin2 ! filesink location=crowd_run_1080p.yuv
+gst-launch -v filesrc location=blue_sky_1080p25.y4m ! decodebin2 ! filesink location=blue_sky_1080p.yuv
 gst-launch -v filesrc location=speed_bag_1080p.y4m ! decodebin2 ! ffmpegcolorspace ! video/x-raw-yuv,format=\(fourcc\)I420 ! filesink location=speed_bag_1080p.yuv
 
 echo ""
 echo "Running 1080p tests"
 echo ""
 
-./haar-test haarcascade_frontalface_alt.xml crowd_run_1080p.yuv 1920 1080 &> crowd_run_1080p.yuv.result
+./haar-test haarcascade_frontalface_alt.xml crowd_run_1080p.yuv 1920 1080 &> results/crowd_run_1080p.result
 rm crowd_run_1080p.yuv
 
-./haar-test haarcascade_frontalface_alt.xml speed_bag_1080p.yuv 1920 1080 &> speed_bag_1080p.yuv.result
-rm speed_bag_1080p.yuv.result
+./haar-test haarcascade_frontalface_alt.xml blue_sky_1080p.yuv 1920 1080 &> results/blue_sky_1080p.result
+rm blue_sky_1080p.yuv
+
+./haar-test haarcascade_frontalface_alt.xml speed_bag_1080p.yuv 1920 1080 &> results/speed_bag_1080p.yuv.result
+rm speed_bag_1080p.yuv
 
 
 echo ""
@@ -55,6 +69,7 @@ echo "Generating 720p YUV videos"
 echo ""
 
 gst-launch -v filesrc location=crowd_run_1080p50.y4m ! decodebin2 ! videoscale ! video/x-raw-yuv,width=1280,height=720 ! filesink location=crowd_run_720p.yuv
+gst-launch -v filesrc location=blue_sky_1080p25.y4m ! decodebin2 ! videoscale ! video/x-raw-yuv,width=1280,height=720 ! filesink location=blue_sky_720p.yuv
 gst-launch -v filesrc location=speed_bag_1080p.y4m ! decodebin2 ! ffmpegcolorspace ! video/x-raw-yuv,format=\(fourcc\)I420 ! videoscale ! video/x-raw-yuv,width=1280,height=720 ! filesink location=speed_bag_720p.yuv
 
 echo ""
@@ -71,6 +86,10 @@ echo ""
 gst-launch -v filesrc location=crowd_run_1080p50.y4m ! decodebin2 ! videoscale ! video/x-raw-yuv,width=320,height=180 ! filesink location=crowd_run_320_180.yuv
 gst-launch -v filesrc location=speed_bag_1080p.y4m ! decodebin2 ! ffmpegcolorspace ! video/x-raw-yuv,format=\(fourcc\)I420 ! videoscale ! video/x-raw-yuv,width=320,height=180 ! filesink location=speed_bag_320_180p.yuv
 
+
+echo ""
+echo "Starting bitrate tests"
+echo ""
 
 # bitrate tests
 echo ""
