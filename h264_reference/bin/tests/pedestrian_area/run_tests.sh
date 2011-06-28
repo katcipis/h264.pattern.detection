d0231bdfd414ceae923ce610eb34ff0b251705ab
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-06-28 10:27:26 -0300

    coisa linda

--- h264_reference/bin/tests/pedestrian_area/run_tests.sh
+++ h264_reference/bin/tests/pedestrian_area/run_tests.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-file pedestrian_area.yuv
-
-if test ! $? -eq 0; then
-    file pedestrian_area_1080p25.y4m
-    if test ! $? -eq 0; then
-        wget http://media.xiph.org/video/derf/y4m/pedestrian_area_1080p25.y4m
-    fi
+if test ! -f pedestrian_area.yuv ; then
+  if test ! -f pedestrian_area_1080p25.y4m ; then
+    wget http://media.xiph.org/video/derf/y4m/pedestrian_area_1080p25.y4m
+  fi
 fi
-
 
 echo "=== Generating YUV file ==="
 
@@ -30,27 +26,27 @@ echo "=== Starting 1/1 test ==="
 
 nice -n -20 ./lencod.exe -f ./tests/pedestrian_area/encoder_1_1.cfg &> ./tests/pedestrian_area/results/encoder_1_1.res
 nice -n -20 ./ldecod.exe -f ./tests/pedestrian_area/decoder.cfg
-nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! avimux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_1_1.avi
+nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_1_1.ogg
 
 
 echo "=== Starting 5/10 test ==="
 
 nice -n -20 ./lencod.exe -f ./tests/pedestrian_area/encoder_5_10.cfg &> ./tests/pedestrian_area/results/encoder_5_10.res
 nice -n -20 ./ldecod.exe -f ./tests/pedestrian_area/decoder.cfg 
-nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! avimux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_5_10.avi
+nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_5_10.ogg
 
 
 echo "=== Starting 10/30 test ==="
 
 nice -n -20 ./lencod.exe -f ./tests/pedestrian_area/encoder_10_30.cfg &> ./tests/pedestrian_area/results/encoder_10_30.res
 nice -n -20 ./ldecod.exe -f ./tests/pedestrian_area/decoder.cfg 
-nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! avimux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_10_30.avi
+nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_10_30.ogg
 
 
 echo "=== Starting 10/60 test ==="
 
 nice -n -20 ./lencod.exe -f ./tests/pedestrian_area/encoder_10_60.cfg &> ./tests/pedestrian_area/results/encoder_10_60.res
 nice -n -20 ./ldecod.exe -f ./tests/pedestrian_area/decoder.cfg 
-nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! avimux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_10_60.avi
+nice -n -20 gst-launch-0.10 filesrc location=./tests/pedestrian_area/pedestrian_area_dec.yuv ! videoparse format=1 width=1920 height=1080 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesrc location=./tests/pedestrian_area/results/pedestrian_area_10_60.ogg
 
 echo "=== We are done ==="
