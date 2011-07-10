8e3126d8290b6f5d4d77db2942c08caa1e0f35a3
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-07-10 15:00:36 -0300

    fixing tests

--- h264_reference/bin/tests/pedestrian_area_720p/run_tests.sh
+++ h264_reference/bin/tests/pedestrian_area_720p/run_tests.sh
@@ -2,13 +2,13 @@
 
 if test ! -f pedestrian_area.yuv ; then
 
-  if test ! -f pedestrian_area_720p25.y4m ; then
-    wget http://media.xiph.org/video/derf/y4m/pedestrian_area_720p25.y4m
+  if test ! -f pedestrian_area1080_p25.y4m ; then
+    wget http://media.xiph.org/video/derf/y4m/pedestrian_area_1080p25.y4m
   fi
 
   echo "=== Generating YUV file ==="
-  gst-launch filesrc location=pedestrian_area_720p25.y4m ! decodebin2 ! ffmpegcolorspace ! videoscale ! video/x-raw-yuv,format=\(fourcc\)I420,width=1280,height=720 ! filesink location=pedestrian_area.yuv
-  rm pedestrian_area_720p25.y4m
+  gst-launch filesrc location=pedestrian_area_1080p25.y4m ! decodebin2 ! ffmpegcolorspace ! videoscale ! video/x-raw-yuv,format=\(fourcc\)I420,width=1280,height=720 ! filesink location=pedestrian_area.yuv
+  rm pedestrian_area_1080p25.y4m
 
 fi
 
@@ -19,50 +19,50 @@ mkdir -p results
 echo "=== Starting original test ==="
 
 cd ../../
-./lencod.exe -f ./tests/pedestrian_area_720/encoder.cfg &> ./tests/pedestrian_area_720/results/encoder_original.res
-cp ./tests/pedestrian_area_720/pedestrian_area.h264 ./tests/pedestrian_area_720/results/pedestrian_area_original.h264
+./lencod.exe -f ./tests/pedestrian_area_720p/encoder.cfg &> ./tests/pedestrian_area_720p/results/encoder_original.res
+cp ./tests/pedestrian_area_720p/pedestrian_area.h264 ./tests/pedestrian_area_720p/results/pedestrian_area_original.h264
 
 echo "=== Starting 1/1 test ==="
 
-./lencod.exe -f ./tests/pedestrian_area_720/encoder_1_1.cfg &> ./tests/pedestrian_area_720/results/encoder_1_1.res
+./lencod.exe -f ./tests/pedestrian_area_720p/encoder_1_1.cfg &> ./tests/pedestrian_area_720p/results/encoder_1_1.res
 
-cp ./tests/pedestrian_area_720/pedestrian_area.h264 ./tests/pedestrian_area_720/results/pedestrian_area_1_1.h264
+cp ./tests/pedestrian_area_720p/pedestrian_area.h264 ./tests/pedestrian_area_720p/results/pedestrian_area_1_1.h264
 
-./ldecod.exe -f ./tests/pedestrian_area_720/decoder.cfg
+./ldecod.exe -f ./tests/pedestrian_area_720p/decoder.cfg
 
-gst-launch-0.10 filesrc location=./tests/pedestrian_area_720/pedestrian_area_dec.yuv ! videoparse format=1 width=1280 height=720 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=./tests/pedestrian_area_720/results/pedestrian_area_1_1.ogg
+gst-launch-0.10 filesrc location=./tests/pedestrian_area_720p/pedestrian_area_dec.yuv ! videoparse format=1 width=1280 height=720 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=./tests/pedestrian_area_720p/results/pedestrian_area_1_1.ogg
 
 
 echo "=== Starting 5/10 test ==="
 
-./lencod.exe -f ./tests/pedestrian_area_720/encoder_5_10.cfg &> ./tests/pedestrian_area_720/results/encoder_5_10.res
+./lencod.exe -f ./tests/pedestrian_area_720p/encoder_5_10.cfg &> ./tests/pedestrian_area_720p/results/encoder_5_10.res
 
-cp ./tests/pedestrian_area_720/pedestrian_area.h264 ./tests/pedestrian_area_720/results/pedestrian_area_5_10.h264
+cp ./tests/pedestrian_area_720p/pedestrian_area.h264 ./tests/pedestrian_area_720p/results/pedestrian_area_5_10.h264
 
-./ldecod.exe -f ./tests/pedestrian_area_720/decoder.cfg 
+./ldecod.exe -f ./tests/pedestrian_area_720p/decoder.cfg 
 
-gst-launch-0.10 filesrc location=./tests/pedestrian_area_720/pedestrian_area_dec.yuv ! videoparse format=1 width=1280 height=720 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=./tests/pedestrian_area_720/results/pedestrian_area_5_10.ogg
+gst-launch-0.10 filesrc location=./tests/pedestrian_area_720p/pedestrian_area_dec.yuv ! videoparse format=1 width=1280 height=720 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=./tests/pedestrian_area_720p/results/pedestrian_area_5_10.ogg
 
 
 echo "=== Starting 10/30 test ==="
 
-./lencod.exe -f ./tests/pedestrian_area_720/encoder_10_30.cfg &> ./tests/pedestrian_area_720/results/encoder_10_30.res
+./lencod.exe -f ./tests/pedestrian_area_720p/encoder_10_30.cfg &> ./tests/pedestrian_area_720p/results/encoder_10_30.res
 
-cp ./tests/pedestrian_area_720/pedestrian_area.h264 ./tests/pedestrian_area_720/results/pedestrian_area_10_30.h264
+cp ./tests/pedestrian_area_720p/pedestrian_area.h264 ./tests/pedestrian_area_720p/results/pedestrian_area_10_30.h264
 
-./ldecod.exe -f ./tests/pedestrian_area_720/decoder.cfg 
+./ldecod.exe -f ./tests/pedestrian_area_720p/decoder.cfg 
 
-gst-launch-0.10 filesrc location=./tests/pedestrian_area_720/pedestrian_area_dec.yuv ! videoparse format=1 width=1280 height=720 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=./tests/pedestrian_area_720/results/pedestrian_area_10_30.ogg
+gst-launch-0.10 filesrc location=./tests/pedestrian_area_720p/pedestrian_area_dec.yuv ! videoparse format=1 width=1280 height=720 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=./tests/pedestrian_area_720p/results/pedestrian_area_10_30.ogg
 
 
 echo "=== Starting 10/60 test ==="
 
-./lencod.exe -f ./tests/pedestrian_area_720/encoder_10_60.cfg &> ./tests/pedestrian_area_720/results/encoder_10_60.res
+./lencod.exe -f ./tests/pedestrian_area_720p/encoder_10_60.cfg &> ./tests/pedestrian_area_720p/results/encoder_10_60.res
 
-cp ./tests/pedestrian_area_720/pedestrian_area.h264 ./tests/pedestrian_area_720/results/pedestrian_area_10_60.h264
+cp ./tests/pedestrian_area_720p/pedestrian_area.h264 ./tests/pedestrian_area_720p/results/pedestrian_area_10_60.h264
 
-./ldecod.exe -f ./tests/pedestrian_area_720/decoder.cfg 
+./ldecod.exe -f ./tests/pedestrian_area_720p/decoder.cfg 
 
-gst-launch-0.10 filesrc location=./tests/pedestrian_area_720/pedestrian_area_dec.yuv ! videoparse format=1 width=1280 height=720 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=./tests/pedestrian_area_720/results/pedestrian_area_10_60.ogg
+gst-launch-0.10 filesrc location=./tests/pedestrian_area_720p/pedestrian_area_dec.yuv ! videoparse format=1 width=1280 height=720 framerate=25/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=./tests/pedestrian_area_720p/results/pedestrian_area_10_60.ogg
 
 echo "=== We are done ==="
