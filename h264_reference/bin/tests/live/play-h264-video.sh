46da19d520aef77c0e8688b271cff3bde8846f3c
Tiago Katcipis <tiagokatcipis@gmail.com>
2012-10-20 17:50:01 -0300

    damn...

--- h264_reference/bin/tests/live/play-h264-video.sh
+++ h264_reference/bin/tests/live/play-h264-video.sh
@@ -1,2 +1,6 @@
+echo ""
+echo "usage: <filename>"
+echo ""
+
 gst-launch-0.10 -v filesrc location=$1 ! decodebin ! ffmpegcolorspace ! videorate ! autovideosink
 
--- h264_reference/bin/tests/live/play-yuv-video.sh
+++ h264_reference/bin/tests/live/play-yuv-video.sh
@@ -1,1 +1,7 @@
+echo ""
+echo "usage: <filename> <width> <height> <framerate>"
+echo ""
+
+echo "Running"
 gst-launch-0.10 filesrc location=$1 ! videoparse format=1 width=$2 height=$3 framerate=$4 ! ffmpegcolorspace ! autovideosink
+echo "Done"
